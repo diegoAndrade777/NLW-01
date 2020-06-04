da6f7d8f97f9ac7e799820a56adc44a2405ef144
Diego Andrade <45301572+diegoAndrade777@users.noreply.github.com>
2020-06-04 16:34:03 -0300

    Frontend update

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,32 +1,11 @@
 import React from 'react';
 import './App.css';
-import logo from '../src/assets/home-background.svg'
+
+import Routes from './routes';
 
 function App() {
   return (
-    <>
-    <div className="App">
-        <div className="content">
-
-            <header>
-                <img src={logo} alt="Logo Ecoleta"/>
-                <a href="#" >
-                    <span></span>
-                    Cadastre seu ponto de coleta
-                </a>
-            </header>
-
-            <main>
-                <h1>Seu marketplace de coleta de resíduos</h1>
-    
-                <p>Ajudamos pessoas a encontarem pontos de coleta de forma eficiente</p>
-    
-                <a href="#" ><span></span><strong>Pesquisar ponto de coleta</strong></a>
-            </main>
-    
-      </div>
-    </div>
-    </>
+    <Routes />
   );
 }
 
